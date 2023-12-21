7575df1811f26e15630af6a9847dc7ef6c44c424
Radhika <radhika.singh@kellton.com>
2023-12-21 12:30:37 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostModule } from './posts/posts.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+@Module({
+  imports: [
+    PostModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      typePaths: ['./**/*.graphql']
+    }),
+  ],
+})
+export class AppModule {}
